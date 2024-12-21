39ea3a778377903bb1a647f3db1a3ed3f87b0a44
Chukwuderah <pleasantchukwu@gmail.com>
2024-12-21 22:29:01 +0100

    make_commit,sh

---
+++ make_commits.sh
@@ -0,0 +1,6 @@
+t status -s | while read -r status file; do
+    if [ "$status" == "M" ] || [ "$status" == "??" ] || [ "$status" == "A" ] || [ "$status" == "D" ]; then
+	            git add "$file"
+		            git commit -m "Update $file"
+			        fi
+			done
